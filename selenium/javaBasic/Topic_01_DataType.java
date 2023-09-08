dcb02a34e0fa5e2b5dddc10888c91315be8811a9
phamthao <phamthihongthao241198@gmail.com>
2023-09-08 07:54:42 +0700

    commit code

---
+++ selenium/javaBasic/Topic_01_DataType.java
@@ -0,0 +1,27 @@
+package javaBasic;
+
+public class Topic_01_DataType {
+
+	public static void main(String[] args) {
+		// Chuỗi
+		String name = "Nguyễn Văn A";
+		
+		// Số nguyên: short/int/long
+		int pupilNumber = 100;
+		
+		// Số thực: fload/ double
+		float price = 123.123f;
+		double money = 321.321d;
+		
+		// Boolean
+		boolean sex = true;
+		
+		System.out.println(name);
+		System.out.println(pupilNumber);
+		System.out.println(price);
+		System.out.println(money);
+		System.out.println(sex);
+
+	}
+
+}
