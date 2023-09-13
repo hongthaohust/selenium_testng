bac6cb9820336ee560f7d8ac252a842a5aaeb712
thao_pham <phamthihongthao241198@gmail.com>
2023-09-13 18:42:14 +0700

    commit code

---
+++ selenium/javaBasic/Topic02_Fake_data.java
@@ -0,0 +1,10 @@
+package javaBasic;
+
+import java.util.Random;
+
+public class Topic02_Fake_data {
+	public static void main( String[] args) {
+		Random rand = new Random();
+		rand.nextInt(999999);
+	}
+}
