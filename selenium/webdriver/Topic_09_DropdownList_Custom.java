5e2b6957580b869d8b3cd9f39fd08461a2e802be
phamthao <phamthihongthao241198@gmail.com>
2023-09-19 23:24:16 +0700

    droplist

--- selenium/webdriver/Topic_09_DropdownList_Custom.java
+++ selenium/webdriver/Topic_09_DropdownList_Custom.java
@@ -35,31 +35,63 @@ public class Topic_09_DropdownList_Custom {
 		expliciWait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		jsExecutor = (JavascriptExecutor) driver;
 		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 	}
 	
 	@Test
-	public void TC_01_Custom_Dropdown1() {
+	public void TC_01_DropDown_JQuery() {
 		driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
 		
 		selectItemDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "5");
 		sleepInSecond(3);
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']/span[text()='5']")).isDisplayed());
 		
-		selectItemDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "11");
+		selectItemDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "10");
 		sleepInSecond(3);
+		//Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']/span[text()='10']")).isDisplayed());
 		
 		selectItemDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "15");
 		sleepInSecond(3);
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']/span[text()='15']")).isDisplayed());
 		
 		selectItemDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "1");
 		sleepInSecond(3);
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']/span[text()='1']")).isDisplayed());
 		
 	}
 
-	@Test
-	public void TC_02() {
+	
+	public void TC_02_DropDown_ReactJS() {
 		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+		
+		
+	}
+	
+	
+	public void TC_03_DropDown_VueJS() {
+		driver.get("");
+		
+		
+	}
+	
+	
+	public void TC_04_DropDown_Editable() {
+		driver.get("");
+		
+		
+	}
+	
+	
+	public void TC_05_DropDown_Angular() {
+		driver.get("");
+		
+		
+	}
+	
+	
+	public void TC_06_DropDown_Multiple_Select() {
+		driver.get("");
 		
 		
 	}
@@ -82,8 +114,10 @@ public class Topic_09_DropdownList_Custom {
 				// 6. Nếu tìm thấy item thì scrol đến item đó
 				jsExecutor.executeScript("arguments[0].scrollIntoView(true)", actualItem);
 				sleepInSecond(1);
+				
 				// 7. Click vào item đó
 				actualItem.click();
+				System.out.println(driver.findElement(By.xpath("//span[@id='number-button']/span[last()]")).getText());
 				
 				// 8. Thoát khỏi vòng lặp
 				break;
