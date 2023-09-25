33fee09ba00c818b5a4b34e160bd14673e0d8d90
thao_pham <phamthihongthao241198@gmail.com>
2023-09-25 15:25:10 +0700

    user_interaction: drag and drop (html4 + html5)

--- selenium/webdriver/Topic_12_User_interaction.java
+++ selenium/webdriver/Topic_12_User_interaction.java
@@ -1,5 +1,11 @@
 package webdriver;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,13 +24,14 @@ import org.testng.annotations.Test;
 
 public class Topic_12_User_interaction {
 	WebDriver driver;
-	JavascriptExecutor jsExecutor;
-	
-	String projectPath = System.getProperty("user.dir");
-	String osName = System.getProperty("os.name");
-	
+	WebElement element;
 	Actions action;
-
+	JavascriptExecutor jsExecutor;
+	String osName = System.getProperty("os.name");
+	String projectPath = System.getProperty("user.dir");
+	String javascriptPath = projectPath + "\\dragAndDrop\\drag_and_drop_helper.js";
+	String jQueryPath = projectPath + "\\dragAndDrop\\jquery_load_helper.js";
+	
 	@BeforeClass
 	public void beforeClass() {
 		if (osName.contains("Windows")) {
@@ -70,6 +77,7 @@ public class Topic_12_User_interaction {
 		
 	}
 
+	
 	public void TC_02_click_and_hold_01() {
 		driver.get("https://automationfc.github.io/jquery-selectable/");
 		
@@ -134,17 +142,75 @@ public class Topic_12_User_interaction {
 		Assert.assertEquals(selectedTextExpected, selectedTextActual);
 	}
 	
-	@Test
+	
 	public void TC_04_double_click() {
 		driver.get("https://automationfc.github.io/basic-form/index.html");
-		WebElement element = driver.findElement(By.xpath("//button[text()='Double click me']"));
+		element = driver.findElement(By.xpath("//button[text()='Double click me']"));
 
 		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
 		sleepInsecond(3);
 		action.doubleClick(element).perform();
 		
 		Assert.assertTrue(driver.findElement(By.xpath("//p[@id='demo']")).isDisplayed());
+	}
+	
+	
+	public void TC_05_right_click() {
+		driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
 		
+		// Right click to button: contextClick()
+		element = driver.findElement(By.xpath("//span[text()='right click me']"));
+		action.contextClick(element).perform();
+		
+		// Hover vào button Quit
+		element = driver.findElement(By.cssSelector(".context-menu-icon-quit"));
+		action.moveToElement(element).perform();
+		
+		// Verify Quit có hover/visible status
+		String quitClassAttribute = element.getAttribute("class");
+		Assert.assertTrue(quitClassAttribute.contains("context-menu-hover"));
+		Assert.assertTrue(quitClassAttribute.contains("context-menu-visible"));
+	}
+	
+	
+	public void TC_06_Drag_and_Drop_HTML4() {
+		driver.get("https://automationfc.github.io/kendo-drag-drop/");
+		
+		WebElement sourceCircle = driver.findElement(By.cssSelector("div#draggable"));
+		WebElement targetCircle = driver.findElement(By.cssSelector("div#droptarget"));
+		
+//		action.dragAndDrop(sourceCircle, targetCircle).perform(); // Cách 1
+		
+		action.clickAndHold(sourceCircle).moveToElement(targetCircle).release().perform(); // Cách 2
+		sleepInsecond(3);
+		
+		Assert.assertEquals(targetCircle.getText(), "You did great!");
+	}
+	
+	@Test
+	public void TC_07_Drag_and_Drop_HTML5() throws IOException {
+		driver.get("https://automationfc.github.io/drag-drop-html5/");
+		
+		String sourceCss = "div#column-a";
+		String targetCss = "div#column-b";
+		
+		String java_script = getContentFile(javascriptPath);
+		
+		// Inject Jquery lib to site - nếu website chưa có jQuery
+//		 String jqueryscript = getContentFile(jQueryPath);
+//		 jsExecutor.executeScript(jqueryscript);
+		
+		// A to B
+		java_script = java_script + "$(\"" + sourceCss + "\").simulateDragDrop({ dropTarget: \"" + targetCss + "\"});";
+		jsExecutor.executeScript(java_script);
+		sleepInsecond(3);
+		Assert.assertTrue(isElementDisplayed("//div[@id='column-a']/header[text()='B']"));
+		
+		// B to A
+		java_script = "$(\"" + sourceCss + "\").simulateDragDrop({ dropTarget: \"" + targetCss + "\"});";
+		jsExecutor.executeScript(java_script);
+		sleepInsecond(3);
+		Assert.assertTrue(isElementDisplayed("//div[@id='column-a']/header[text()='A']"));
 	}
 
 	@AfterClass
@@ -159,7 +225,32 @@ public class Topic_12_User_interaction {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	}
+	
+	public String getContentFile(String filePath) throws IOException {
+		Charset cs = Charset.forName("UTF-8");
+		FileInputStream stream = new FileInputStream(filePath);
+		try {
+			Reader reader = new BufferedReader(new InputStreamReader(stream, cs));
+			StringBuilder builder = new StringBuilder();
+			char[] buffer = new char[8192];
+			int read;
+			while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+				builder.append(buffer, 0, read);
+			}
+			return builder.toString();
+		} finally {
+			stream.close();
+		}
+	}
+	
+	public boolean isElementDisplayed(String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		if(element.isDisplayed()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
